acc869fdaa78a83e7bfb3e7af1a736c6c98e3e43
KLab-UT <utt.klab@gmail.com>
2024-11-07 11:31:57 -0700

    added script to download human genome

---
+++ get_genomes.sh
@@ -0,0 +1,3 @@
+# This script will download the genome of the specified file from GenBank's refseq database
+
+rsync rsync://ftp.ncbi.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/all_assembly_versions/GCF_000001405.26_GRCh38/GCF_000001405.26_GRCh38_rna.fna.gz .
